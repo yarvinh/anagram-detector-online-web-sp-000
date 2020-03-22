65b739dfb533f7e3acef641a47b6cac97f4b774f
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-22 19:15:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,11 +7,12 @@ class Anagram
   end
   def match(match)
     #  match.sort
-
-     if match.index(@word) == nil
-       @match = []
-     else
-      @match << match.index(@word)
-    end
+    @word
+    #
+    #  if match.index(@word) == nil
+    #    @match = []
+    #  else
+    #   @match << match.index(@word)
+    # end
      end
 end
